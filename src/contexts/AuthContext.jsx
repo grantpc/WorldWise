df00c2d31bc2df1a529c43a478d3f6fb5e65dc5a
Grant Christensen <59584917+grantpc@users.noreply.github.com>
2023-06-24 11:17:19 -0700

    Implement fake authentication and protected routes

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,59 @@
+import { useReducer } from 'react';
+import { useContext, createContext } from 'react';
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'login':
+      console.log('logged in!');
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case 'logout':
+      console.log('logged out!');
+      return { ...state, user: null, isAuthenticated: false };
+
+    default:
+      throw new Error('Unknown action type');
+  }
+}
+
+const FAKE_USER = {
+  name: 'Jack',
+  email: 'jack@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=zz',
+};
+
+export function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: 'login', payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: 'logout' });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error('AuthContext was used outside of AuthProvider.');
+
+  return context;
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,31 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import PageNav from '../components/PageNav';
+import { useAuth } from '../contexts/AuthContext';
 import styles from './Login.module.css';
 
 export default function Login() {
+  const { isAuthenticated, login } = useAuth();
+  const navigate = useNavigate();
+
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState('jack@example.com');
   const [password, setPassword] = useState('qwerty');
 
+  useEffect(() => {
+    if (isAuthenticated) navigate('/app');
+  }, [isAuthenticated, navigate]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    login(email, password);
+  };
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
